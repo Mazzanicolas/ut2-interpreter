87b781dde0d5aa6a8f875468ed5673f3620c78cd
Nicolás Mazza <mazzanicolas@gmail.com>
2017-09-05 19:25:24 -0300

    add: string*number

--- src/ast/Multiplication.ts
+++ src/ast/Multiplication.ts
@@ -23,7 +23,14 @@ export class Multiplication implements Exp {
   }
 
   evaluate(state: State): any {
-    var result:number = this.lhs.evaluate(state) * this.rhs.evaluate(state);
+    var result:number=0;
+    if (typeof this.lhs.evaluate(state)!='number'){
+      result= this.lhs.evaluate(state).repeat(this.rhs.evaluate(state));
+    } else if(typeof this.rhs.evaluate(state)!='number'){
+      result= this.rhs.evaluate(state).repeat(this.lhs.evaluate(state));
+    }else{
+      result = this.lhs.evaluate(state) * this.rhs.evaluate(state);
+    }
     return result;
   }
 }
